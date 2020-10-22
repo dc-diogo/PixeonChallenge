d968386a756720cc3df317b96736b940274882b5
Diogo Viana <diogocampos@outlook.com>
2020-10-22 00:42:37 -0300

    Changed API code that is returned when user send invalid Health Care Institution

--- src/main/java/com/pixeon/challenge/createhealthcare/CreateHealthCareInstitutionService.java
+++ src/main/java/com/pixeon/challenge/createhealthcare/CreateHealthCareInstitutionService.java
@@ -2,6 +2,8 @@ package com.pixeon.challenge.createhealthcare;
 
 import com.pixeon.challenge.createhealthcare.domain.HealthCareInstitutionDomain;
 import com.pixeon.challenge.datastore.DataStore;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,30 +12,21 @@ public final class CreateHealthCareInstitutionService {
     private final HealthCareInstitutionRepository repository;
     private final HealthCareInstitutionValidator validator;
 
-    //APAGAR
-    private final DataStore dataStore;
-
-    public CreateHealthCareInstitutionService(HealthCareInstitutionRepository repository, HealthCareInstitutionValidator validator, DataStore dataStore) {
+    public CreateHealthCareInstitutionService(HealthCareInstitutionRepository repository, HealthCareInstitutionValidator validator) {
         this.repository = repository;
         this.validator = validator;
-        this.dataStore = dataStore;
-    }
-
-    @GetMapping("hello")
-    public String hello(@RequestParam String name) {
-        return dataStore.getByCNPJ(name).getCnpj();
     }
 
     @PostMapping("hc/create")
-    public String create( @RequestBody() HealthCareInstitution institution) {
+    public ResponseEntity<String> create(@RequestBody() HealthCareInstitution institution) {
         final HealthCareInstitutionDomain domainInstitution = new HealthCareInstitutionDomain(institution.getName(), institution.getCnpj(), 20);
 
         if (validator.validate(domainInstitution)){
             repository.save(domainInstitution);
-            return "Salvou a clinica";
+            return new ResponseEntity<>("Institution saved successfully.", HttpStatus.BAD_REQUEST);
         }
 
-        return "Deu errado";
+        return new ResponseEntity<>("Invalid institution, please check and try again", HttpStatus.OK);
 
     }
 }
